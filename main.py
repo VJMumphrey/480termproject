a5d3775f9e4d50cf8958bd8383fa35e51d357f66
VJMumphrey <vjm007@latech.edu>
2024-05-20 22:47:23 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+import sys
+
+# open file for reading
+f = open(sys.argv[1], "r")
+
+# Start of the Reasearch Paper
+
+# create slice
+
+# tokenisation
+
+# parsing
+
+# output the resulting XML document
