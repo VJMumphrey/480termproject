7c62c21e688e58a9bdddf9505d0a7f88e5911096
VJMumphrey <vjm007@latech.edu>
2024-05-21 22:33:36 -0500

    push stub code

--- main.py
+++ main.py
@@ -1,15 +1,56 @@
+# Get test info used to compare results
 
-import sys
-
-# open file for reading
-f = open(sys.argv[1], "r")
 
 # Start of the Reasearch Paper
 
-# create slice
+# parent class for all sections in a paper
+# this should encapsulate set of N
+class sectionData:
+    def __init__(self, sectionData):
+        self.sectionData = sectionData
 
-# tokenisation
+    def getSectionData(self, sectionData):
+        return sectionData
 
-# parsing
+    def setSectionData(self, sectionData):
+        self.sectionData = sectionData
 
-# output the resulting XML document
+
+# P_p
+class preliminary_parts(sectionData):
+    def __init__(self, sectionData):
+        super().__init__(sectionData)
+
+
+# P_c
+class chapter_parts(sectionData):
+
+    def __init__(self, sectionData):
+        self.sectionData = []
+        super().__init__(sectionData)
+
+
+# P_a
+class appendix_parts(sectionData):
+    def __init__(self, sectionData):
+        self.sectionData = []
+        super().__init__(sectionData)
+
+
+# P_r
+class reference_parts(sectionData):
+    def __init__(self, sectionData):
+        super().__init__(sectionData)
+
+
+# parent class for all tokens
+# should encapsulate all tokens in t
+class tokens:
+    def __init__(self, tokenData) -> None:
+        self.tokenData = tokenData
+
+    def getTokenData(self, tokenData):
+        return tokenData
+
+    def setTokenData(self, tokenData):
+        self.tokenData = tokenData
