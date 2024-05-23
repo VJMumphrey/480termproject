73619d2e4f662b1d7b268f46b8ae35e9a05bacfb
VJMumphrey <vjm007@latech.edu>
2024-05-22 21:00:14 -0500

    write code for paper

--- main.py
+++ main.py
@@ -1,56 +1,80 @@
-# Get test info used to compare results
+import threading
+
+ex1data = "aaa"
+ex2data = "abababaab"
+
+thread1 = False
+thread2 = False
+thread3 = False
+
+# List thats keeps track of thread results
+boolList = [thread1, thread2, thread3]
 
 
-# Start of the Reasearch Paper
-
-# parent class for all sections in a paper
-# this should encapsulate set of N
-class sectionData:
-    def __init__(self, sectionData):
-        self.sectionData = sectionData
-
-    def getSectionData(self, sectionData):
-        return sectionData
-
-    def setSectionData(self, sectionData):
-        self.sectionData = sectionData
+# Generate tests
+def example1(index: int, char: str) -> None:
+    # do the threading for a FA
+    if char == 'a':
+        boolList[index] = True
 
 
-# P_p
-class preliminary_parts(sectionData):
-    def __init__(self, sectionData):
-        super().__init__(sectionData)
+def example2A(index: int, char: str) -> None:
+    # do the threading for a FA
+    if char == 'a':
+        boolList[index] = True
 
 
-# P_c
-class chapter_parts(sectionData):
-
-    def __init__(self, sectionData):
-        self.sectionData = []
-        super().__init__(sectionData)
+def example2B(index: int, char: str) -> None:
+    # do the threading for a FA
+    if char == 'b':
+        boolList[index] = True
 
 
-# P_a
-class appendix_parts(sectionData):
-    def __init__(self, sectionData):
-        self.sectionData = []
-        super().__init__(sectionData)
+if __name__ == "__main__":
+    # L1(aaa), this requires us to use three threads
+    # this is a simple NFA test
+    threads = list()
+    for index in range(3):
+        i = ex1data[index]
+        x = threading.Thread(target=example1, args=(index, i,))
+        threads.append(x)
+        x.start()
 
+    for index, thread in enumerate(threads):
+        thread.join()
 
-# P_r
-class reference_parts(sectionData):
-    def __init__(self, sectionData):
-        super().__init__(sectionData)
+    if thread1 and thread2 and thread3:
+        print("example 1 accepted")
 
+    for i in range(0, len(boolList) - 1):
+        boolList[i] = False
 
-# parent class for all tokens
-# should encapsulate all tokens in t
-class tokens:
-    def __init__(self, tokenData) -> None:
-        self.tokenData = tokenData
+    # L2((a,b)*aba(a,b)*),
+    # this requires us to use three threads to check for aba
+    threads = list()
+    for outIndex in range(len(ex2data)):
+        if ex2data[outIndex] is None:
+            print("rejected")
+            break
+        i = ""
+        for index in range(3):
+            i = ex2data[index]
 
-    def getTokenData(self, tokenData):
-        return tokenData
+            if index == 2:
+                x = threading.Thread(target=example2B, args=(index, i,))
+            else:
+                x = threading.Thread(target=example2A, args=(index, i,))
 
-    def setTokenData(self, tokenData):
-        self.tokenData = tokenData
+            threads.append(x)
+            x.start()
+
+        for index, thread in enumerate(threads):
+            thread.join()
+
+        if thread1 and thread2 and thread3:
+            print("example 2 accepted")
+        elif i == 'a' or i == 'b':
+            continue
+        else:
+            print("rejected")
+            break
